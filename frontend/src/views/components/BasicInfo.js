48f2a64b00fc9fa138b4f37d0b7fb03a1a8b4aae
Dennis <zhang_dennis1999@hotmail.com>
2021-02-07 04:14:00 -0800

    added lottie animation for profile

--- frontend/src/views/components/BasicInfo.js
+++ frontend/src/views/components/BasicInfo.js
@@ -1,17 +1,27 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import "../styles/BasicInfo.css";
 import { storage } from "../../models/storage";
+import lottie from "lottie-web";
 
 const BasicInfo = () => {
   // var tid = storage("tid");
   // var rmp_link = "https://www.ratemyprofessors.com/ShowRatings.jsp?tid=" + tid;
   var myResponse = storage("responseData");
+  const animate_profile = useRef(null);
+
+  useEffect(() => {
+    lottie.loadAnimation({
+      container: animate_profile.current,
+      renderer: "svg",
+      loop: false,
+      autoplay: true,
+      animationData: require("./animate_profile.json"),
+    });
+  }, []);
 
   return (
     <div className="basic-info-container">
-      <div>
-        <img className="rmp-img" alt="rmp-logo" src="images/rmp-1.png" />
-      </div>
+      <div className="animate-profile" ref={animate_profile}></div>
       <h1>{myResponse.name}</h1>
       <div className="prof-link-container">
         <a
