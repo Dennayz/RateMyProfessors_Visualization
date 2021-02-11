5615bd3b8f093cd6321b0eaf4230a1117ad3e602
Dennis <zhang_dennis1999@hotmail.com>
2021-02-11 00:03:20 -0800

    set up review component

---
+++ frontend/src/views/components/ReviewSection.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "../styles/ReviewSection.css";
+
+const ReviewSecton = () => {
+  return (
+    <div className="review-section-container">
+      <div className="review-wrapper">
+        <div className="review-header-wrapper">
+          <h2 className="review-header">Top Reviews</h2>
+        </div>
+        <div className="review-section-div">
+          here are all reviews here are all reviews here are all reviews here
+          are all reviews here are all reviews here are all reviews here are all
+          reviews here are all reviews here are all reviews here are all reviews
+          here are all reviews here are all reviews here are all reviews here
+          are all reviews here are all reviews here are all reviews here are all
+          reviews here are all reviews
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ReviewSecton;
--- frontend/src/views/pages/Professor.js
+++ frontend/src/views/pages/Professor.js
@@ -1,12 +1,14 @@
 import React from "react";
 import BasicInfo from "../components/BasicInfo";
 import Charts from "../components/Charts";
+import ReviewSecton from "../components/ReviewSection";
 
 const Professor = () => {
   return (
     <>
       <BasicInfo />
       <Charts />
+      <ReviewSecton />
     </>
   );
 };
