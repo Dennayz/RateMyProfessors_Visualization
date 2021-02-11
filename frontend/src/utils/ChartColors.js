a1e12d8077a07b912430cc8293d0d7a6726272ef
Dennis <zhang_dennis1999@hotmail.com>
2021-02-10 20:39:12 -0800

    updated chart colors components

--- frontend/src/utils/ChartColors.js
+++ frontend/src/utils/ChartColors.js
@@ -1,3 +1,31 @@
-const ChartColors = () => {};
+const backgroundColor = [
+  "rgba(255, 99, 132, 0.7)",
+  "rgba(54, 162, 235, 0.7)",
+  "rgba(255, 206, 86, 0.7)",
+  "rgba(75, 192, 192, 0.7)",
+  "rgba(153, 102, 255, 0.7)",
+  "rgba(255, 159, 64, 0.7)",
+];
 
-export default ChartColors;
+const borderColor = [
+  "rgba(255, 99, 132, 1)",
+  "rgba(54, 162, 235, 1)",
+  "rgba(255, 206, 86, 1)",
+  "rgba(75, 192, 192, 1)",
+  "rgba(153, 102, 255, 1)",
+  "rgba(255, 159, 64, 1)",
+];
+
+const sentimentAnalysisColor = [
+  {
+    positive: "rgba(54, 162, 235, 0.7)",
+  },
+  {
+    negative: "rgba(255, 99, 132, 0.7)",
+  },
+  {
+    neutral: "rgba(255, 206, 86, 0.7)",
+  },
+];
+
+export { backgroundColor, borderColor, sentimentAnalysisColor };
