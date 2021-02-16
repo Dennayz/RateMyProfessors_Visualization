6b90ac25f4a80255614f3403716fd95ecdfbcebd
Dennis <zhang_dennis1999@hotmail.com>
2021-02-16 00:40:40 -0800

    removed commented out code from app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,39 +19,3 @@ function App() {
 }
 
 export default App;
-
-// var Sentiment = require("sentiment");
-
-// var sentiment = new Sentiment();
-//   const [commentsList, setCommentsList] = useState([]);
-//   const [displayMessage, setDisplayMessage] = useState(null);
-
-// const handleButtonclick = async () => {
-//   var tidInput = document.getElementById("tid").value;
-//   try {
-//     const resp = await axios.post("/professor", {
-//       tid: tidInput,
-//     });
-//     console.log(resp.data.reviews);
-//     setDisplayMessage(resp.data.name);
-//     setCommentsList(resp.data.reviews);
-//   } catch (error) {
-//     console.error("no professor found");
-//   }
-// };
-
-//   return (
-//     <div className="App">
-//       <input id="tid" className="tid" placeholder="tid=1442290"></input>
-//       <button onClick={handleButtonclick}>Get Response</button>
-//       <div>{displayMessage}</div>
-//       <div>
-//         {commentsList.map((comment, index) => (
-//           <div key={index}>
-//             <section>{comment.comment}</section>
-//             <br />
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   );
