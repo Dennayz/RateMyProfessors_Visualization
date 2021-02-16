e39abbea8cece1d8b1344c0a2e812b699619d761
Dennis <zhang_dennis1999@hotmail.com>
2021-02-16 00:23:49 -0800

    added quality and difficulty subheaders and finished css conditions

--- frontend/src/views/components/ReviewCard.js
+++ frontend/src/views/components/ReviewCard.js
@@ -2,12 +2,43 @@ import React from "react";
 import "../styles/ReviewCard.css";
 
 const ReviewCard = ({ comment }) => {
+  var qualityColor,
+    difficultyColor = null;
+
+  if (comment.quality >= 0 && comment.quality < 3) {
+    qualityColor = "red";
+  } else if (comment.quality > 3) {
+    qualityColor = "green";
+  } else {
+    qualityColor = "rgb(255,183,0)";
+  }
+
+  if (comment.difficulty >= 0 && comment.difficulty < 3) {
+    difficultyColor = "green";
+  } else if (comment.difficulty > 3) {
+    difficultyColor = "red";
+  } else {
+    difficultyColor = "rgb(255,183,0)";
+  }
   return (
     <div className="comment-container">
       <div className="heading-wrapper">
         <span className="heading-div">{comment.course}</span>
-        <span className="heading-div-second">{comment.quality}</span>
-        <span className="heading-div-third">{comment.difficulty}</span>
+        <div className="quality-div">
+          <span className="subheader">Quality: </span>
+          <span className="heading-div-second" style={{ color: qualityColor }}>
+            {comment.quality}
+          </span>
+        </div>
+        <div className="difficulty-div">
+          <span className="subheader">Difficulty: </span>
+          <span
+            className="heading-div-third"
+            style={{ color: difficultyColor }}
+          >
+            {comment.difficulty}
+          </span>
+        </div>
         <span className="heading-div-fourth">{comment.time_stamp}</span>
       </div>
       <section className="comment-section">{comment.comment}</section>
