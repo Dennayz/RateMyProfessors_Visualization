c2e9d16b321b476b2f821aafbbaf844216ce79ba
Dennis <zhang_dennis1999@hotmail.com>
2021-02-28 22:08:12 -0800

    created a history page

---
+++ frontend/src/views/pages/History.js
@@ -0,0 +1,12 @@
+import React from "react";
+import HistoryTable from "../components/HistoryTable";
+
+const History = () => {
+  return (
+    <>
+      <HistoryTable />
+    </>
+  );
+};
+
+export default History;
