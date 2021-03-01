88754b2d03f64088c3475cc6c5d45a24e5da5bd9
Dennis <zhang_dennis1999@hotmail.com>
2021-02-28 22:12:59 -0800

    created controller folder

--- frontend/src/views/components/HistoryTable.js
+++ frontend/src/views/components/HistoryTable.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 
 const HistoryTable = () => {
+  const [professorsList, setProfessorsList] = useState([]);
   return <div>history table</div>;
 };
 
