f30f123d9361a8af211093513db45e91e6e8356b
Dennis <zhang_dennis1999@hotmail.com>
2021-02-28 22:10:18 -0800

    addded history route in app page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { HashRouter, Switch, Route } from "react-router-dom";
 import Search from "./views/pages/Search";
 import Professor from "./views/pages/Professor";
 import Error from "./views/pages/Error";
+import History from "./views/pages/History";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" exact component={Search} />
           <Route path="/Professor" component={Professor} />
           <Route path="/Error" component={Error} />
+          <Route path="/History" component={History} />
         </Switch>
       </HashRouter>
     </>
---
+++ frontend/src/views/components/HistoryTable.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HistoryTable = () => {
+  return <div>history table</div>;
+};
+
+export default HistoryTable;
